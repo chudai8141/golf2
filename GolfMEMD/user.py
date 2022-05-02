5130184b70e0e505336e3d86d4ebf9405753fb68
chudai <chu.dai.kimukimu.0420@gmail.com>
2022-05-02 16:31:51 +0900

    create user.py

---
+++ GolfMEMD/user.py
@@ -0,0 +1,46 @@
+import os
+import copy
+from typing import List, Dict
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from joint import Joint
+
+
+class User():
+    
+    def __init__(self, username: str, set_joint: Dict[int, str], ballistic: str, impact_list: List[int], last_frame: int):
+        self.user = username
+        self.joint = set_joint
+        self.joint_name = set_joint
+        self.ballistic = ballistic # ballistic -> 弾道の意味
+        self.impact_list = impact_list
+        self.last_frame = last_frame
+        self.output(self, output_dir='output_image')
+    
+    def output(self, output_dir='output_image'):
+        self.output_dir = output_dir
+        if not os.path.isdir(self.output_dir):
+            os.mkdir(self.output_dir)
+        if not os.path.isdir(os.path.join(self.output_dir, self.user)):
+            os.mkdir(os.path.join(self.output_dir, self.user))
+        if not os.path.isdir(os.path.join(self.output_dir, self.user, self.ballistic)):
+            os.mkdir(os.path.join(self.output_dir, self.user, self.ballistic))
+        if not os.path.isdir(os.path.join(self.output_dir, self.user, self.ballistic, self.joint)):
+            os.mkdir(os.path.join(self.output_dir, self.user, self.ballistic, self.joint))
+        save_path = os.path.join(self.output_dir, self.user, self.ballistic, self.joint_name)
+        print(save_path)
+    
+    def _update_joint(self, select_joint):
+        self.joint = select_joint
+
+    def _update_ballistic(self, ballistic):
+        self.ballistic = ballistic
+
+    def update_content(self, select_joint=None, ballistic=None):
+        if select_joint is not None:
+            self._update_joint(self, select_joint)
+        if ballistic is not None:
+            self._update_ballistic(self, ballistic)
+
