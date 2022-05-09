2e2707be4d00939d13c46e43fb9c7b29469b2fed
chudai <chu.dai.kimukimu.0420@gmail.com>
2022-05-09 14:57:54 +0900

    update code for HilbertTrans

--- GolfMEMD/hht.py
+++ GolfMEMD/hht.py
@@ -19,14 +19,12 @@ def get_data(data_path: str, set_joint: dict):
 
 
 class MultEmpModeDeco:
-    def __init__(self, data, set_joint, flag_sep: bool=True):
+    def __init__(self, data, dt, set_joint, flag_sep: bool=True):
         self.data = data
+        self.dt = dt
         self.joint = set_joint['j_num']
         self.joint_name = set_joint['j_name']
-        self.imf = self.get_memd(self.data)
-        self.result_x = None
-        self.result_y = None
-        self.result_z = None
+        self.get_memd()
 
         if flag_sep:
             self.sep_dim()
@@ -40,6 +38,24 @@ class MultEmpModeDeco:
         self.result_y = self.imf[:, self.joint+2, :]
         
 
-
 class HilbertTrans:
-    def __init__(self, MultEmpModeDeco):
+    def __init__(self, result_memd: MultEmpModeDeco):
+        self.result = result_memd
+        self.hilbert_transform()
+        self.freq_list = np.array([self.freq_x, self.freq_z, self.freq_y])
+        self.amp_list = np.array([self.amp_x, self.amp_z, self.amp_y])
+
+
+    def hilbert_transform(self):
+        self.freq_x, self.amp_x = ht.FAhilbert(self.result.result_x, self.result.dt)
+        self.freq_z, self.amp_z = ht.FAhilbert(self.result.result_z, self.result.dt)
+        self.freq_y, self.amp_y = ht.FAhilbert(self.result.result_y, self.result.dt)
+
+        self.freq_x, self.freq_z, self.freq_y = self.freq_x.T, self.freq_z.T, self.freq_y.T
+        self.amp_x, self.amp_z, self.amp_y = self.amp_x.T, self.amp_z.T, self.amp_y.T
+
+    def calc_means_norm(self):
+        # calculation frequency average from freq_list and, amplitude normalize from amp_list
+        self.freq_mean = np.mean(self.freq_list, axis=0)
+        self.amp_norm = LA.norm(self.amp_list, axis=0)
+
