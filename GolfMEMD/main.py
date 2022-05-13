e72d808885c453bbe07f6c54466912871f16aedb
chudai <chu.dai.kimukimu.0420@gmail.com>
2022-05-13 16:19:27 +0900

    update hht_calculation.ipynb and main.py and, create plot.py using main.py plotting

--- GolfMEMD/main.py
+++ GolfMEMD/main.py
@@ -1,8 +1,87 @@
-import os
-import copy
-
-import numpy as np
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from joint import Joint
+from user_setting import impact_number, Sugawara, Kimura
+from user import User
+from hht import get_data, MultEmpModeDeco, HilbertTrans, freq_amp_mean_norm, create_spectrum_time
+from plot import plot
+
+
+set_joint = Joint().hip
+
+select_data = Sugawara().slice_data
+ballistic = select_data['select_data']
+impact_list = select_data['impact_list']
+follor_throught = select_data['follor_throught']
+
+output = 'output_image'
+user_name = 'sugawara'
+user = User(
+    user_name=user_name,
+    set_joint=set_joint,
+    ballistic=ballistic,
+    impact_list=impact_list,
+    last_frame=follor_throught
+)
+
+
+# calculation memd
+data_path = select_data['data_path']
+result_memd_list = []
+
+print('caluculation memd', user.user, user.ballistic, user.joint_name)
+for data in tqdm(data_path):
+    #
+    data, dt, text = get_data(data, set_joint)
+    result_memd = MultEmpModeDeco(data=data, dt=dt, set_joint=set_joint)
+    result_memd_list.append(result_memd)
+
+Nod = 6
+resutl_hilbert_list = []
+
+print('hilbert transform')
+for i, result_memd in tqdm(enumerate(result_memd_list)):
+    result_ht = HilbertTrans(result_memd=result_memd, select_data=select_data)
+    result_ht.calc_means_norm()
+    result_ht.set_freq_data(Nod=Nod, impact_number=impact_number[i])
+    result_ht.set_amp_data(Nod=Nod, impact_number=impact_number[i])
+    resutl_hilbert_list.append(result_ht)
+
+freq_all_data, amp_norm_data = freq_amp_mean_norm(result_hilbert_list=resutl_hilbert_list)
+
+# create spectrum time
+frame = freq_all_data.shape[1]
+spectrum_time = create_spectrum_time(Nod=Nod, frame=frame, dt=dt)
+
+# output plot
+print('output plot')
+for n in tqdm(range(1, Nod+1)):
+    if n == 0:
+        continue
+
+    plot(
+    spectrum_time=spectrum_time,
+    freq_all_data=freq_all_data,
+    amp_all_data=amp_norm_data,
+    Nod=Nod,
+    frame=frame,
+    start=n-1,
+    end=n,
+    joint_name=set_joint['j_name'],
+    select_data=select_data['select_data'],
+    save_path=user.save_path
+    )
+
+    plot(
+    spectrum_time=spectrum_time,
+    freq_all_data=freq_all_data,
+    amp_all_data=amp_norm_data,
+    Nod=Nod,
+    frame=frame,
+    start=0,
+    end=n,
+    joint_name=set_joint['j_name'],
+    select_data=select_data['select_data'],
+    save_path=user.save_path    
+    )
 
---
+++ GolfMEMD/plot.py
@@ -0,0 +1,35 @@
+import os
+
+from matplotlib import pyplot as plt
+
+
+imf_name = ['IMF1', 'IMF2', 'IMF3', 'IMF4', 'IMF5', 'IMF6', 'IMF7', 'IMF8']
+
+
+def plot(
+    spectrum_time,
+    freq_all_data,
+    amp_all_data,
+    Nod,
+    frame,
+    start,
+    end,
+    joint_name,
+    select_data,
+    save_path
+):
+    plt.clf()
+    plt.figure(dpi=200, figsize=(16, 9))
+    plt.rcParams['font.family'] = 'Times new Roman'
+    plt.rcParams['font.size'] = 30
+    for n in range(start, end):
+        plt.scatter(spectrum_time[n, :], freq_all_data[n, :], s = 100, c=amp_all_data[n, :frame], cmap='jet')
+    ax = plt.gca()
+    ax.set_facecolor([0.0, 0.0, 0.5])
+    plt.ylim(0, 10)
+    plt.xlabel('time [s]')
+    plt.ylabel('frequency [Hz]')
+    plt.colorbar()
+    file_name = '_'.join(imf_name[start:end])
+    print('save figure', os.path.join(save_path, file_name) )
+    plt.savefig(os.path.join(save_path, file_name))
